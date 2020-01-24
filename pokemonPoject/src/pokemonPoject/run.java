06a57bd7ec4c1fb8e6a1285f48f322d5fbcfc232
Predator <Predator@DESKTOP-CIBGTRG>
2020-01-24 08:17:15 -0600

    even more testing

--- pokemonPoject/src/pokemonPoject/run.java
+++ pokemonPoject/src/pokemonPoject/run.java
@@ -5,6 +5,7 @@ public class run {
 	public static void main(String[] args) {
 		System.out.println("I hope this works plesaseee");
 		System.out.println("did this work?");
+		System.out.println("IT DID WORK");
 	}
 
 }
