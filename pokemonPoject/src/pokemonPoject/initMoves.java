874b94db2c6dae02f71b426d5f502c665b05c9e6
Predator <Predator@DESKTOP-CIBGTRG>
2020-01-27 09:23:04 -0600

    thunder1

--- pokemonPoject/src/pokemonPoject/initMoves.java
+++ pokemonPoject/src/pokemonPoject/initMoves.java
@@ -4,4 +4,5 @@ public class initMoves {
 	//move name = new move(name, type, atkType, atk, hitChance, pp);
 	move thunderbolt = new move("Thunderbolt", "Electric", "Special", 90, 100, 15);
 	move thunder = new move("Thunder", "Electric", "Special", 110, 70, 10);
+	move thunder1 = new move("Thunder", "Electric", "Special", 110, 70, 10);
 }
