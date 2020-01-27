b7f936a8da54f5eec1781eddc01c6dddc3ae0616
joshu <joshu@LAPTOP-Q2V7DVES>
2020-01-27 09:20:48 -0600

    added thunder in initMoves

--- pokemonPoject/src/pokemonPoject/initMoves.java
+++ pokemonPoject/src/pokemonPoject/initMoves.java
@@ -3,4 +3,5 @@ package pokemonPoject;
 public class initMoves {
 	//move name = new move(name, type, atkType, atk, hitChance, pp);
 	move thunderbolt = new move("Thunderbolt", "Electric", "Special", 90, 100, 15);
+	move thunder = new move("Thunder", "Electric", "Special", 110, 70, 10);
 }
